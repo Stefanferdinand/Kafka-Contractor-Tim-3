e8dd0a176078f33b0677709020637f8c1042b891
STEFAN FERDINAND <stefan.ferdinand75@gmail.com>
2024-07-30 21:33:51 +0700

    initial commit

---
+++ KafkaOnDemandContractor/src/main/java/com/bca/kafka/KafkaOnDemandContractor/KafkaOnDemandContractorApplication.java
@@ -0,0 +1,13 @@
+package com.bca.kafka.KafkaOnDemandContractor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaOnDemandContractorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaOnDemandContractorApplication.class, args);
+	}
+
+}
---
+++ KafkaOnDemandContractor/src/test/java/com/bca/kafka/KafkaOnDemandContractor/KafkaOnDemandContractorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bca.kafka.KafkaOnDemandContractor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaOnDemandContractorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
