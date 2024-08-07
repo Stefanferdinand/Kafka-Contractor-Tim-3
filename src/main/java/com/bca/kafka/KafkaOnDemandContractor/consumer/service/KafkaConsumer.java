8d192949a89705c8dad9a4e825999d1b3fa008c3
Vincent <vincenthartantoo.work@gmail.com>
2024-08-07 22:57:05 +0700

    Add Kafka Consumer

---
+++ src/main/java/com/bca/kafka/KafkaOnDemandContractor/consumer/service/KafkaConsumer.java
@@ -0,0 +1,13 @@
+package com.bca.kafka.KafkaOnDemandContractor.consumer.service;
+
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+@Service
+public class KafkaConsumer {
+
+    @KafkaListener(topics = "contractor-position-and-rate", groupId = "CG_CONTRACTTOR_USER_RATE")
+    public void consume(String message) {
+        System.out.println("Analytic Constructor Rate : " + message);
+    }
+}
